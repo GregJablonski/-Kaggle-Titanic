81dc0fb2608bd9678fb75bcb85b743bd906b7ff8
GregJablonski <gmjablonski@gmail.com>
2017-07-27 16:38:08 +0200

    Feature Engineering Please enter the commit message for your changes. Lines starting

---
+++ Titanic.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(stringr)
+setwd("C:\\Users\\grjablon\\Desktop\\git\\KaggleTitanic")
+
+
+train <- read_csv("train.csv")
+test <- read_csv("test.csv")
+full <- bind_rows(train, test)
+
+full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
+table(full$Sex, full$Title)
+
+Rare_title <- c("Capt", "Col", "Don", "Dona", "Dr", "Jonkheer", "Lady", "Major", "Rev", "Sir", "the Countess")
+
+full$Title[full$Title == "Mlle"] <- "Miss" 
+full$Title[full$Title == "Mme"] <- "Mrs" 
+full$Title[full$Title == "Ms"] <- "Miss" 
+full$Title[full$Title %in% Rare_title] <- "Rare Title" 
+
+full$Surname <- str_extract(full$Name, '^([^,]*)' )
+
+full$Family <- full$SibSp
+
+full$Family[full$Family >= 2] <- "Big Family"
+full$Family[full$Family == 0] <- "Single"
+full$Family[full$Family == 1] <- "Pair"
+
+full$Deck <- sapply(full$Cabin, function(x) strsplit(x, NULL)[[1]][1])
